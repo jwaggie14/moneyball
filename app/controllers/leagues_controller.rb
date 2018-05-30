6093ceaa57edb94ba365735e8c0fa1c814518410
Raghu Betina <raghu@firstdraft.com>
2018-05-30 01:07:45 +0000

    Only league user can access show page

--- app/controllers/leagues_controller.rb
+++ app/controllers/leagues_controller.rb
@@ -1,5 +1,5 @@
 class LeaguesController < ApplicationController
-  before_action :current_user_must_be_league_user, :only => [:edit, :update, :destroy]
+  before_action :current_user_must_be_league_user, :only => [:show, :edit, :update, :destroy]
 
   def current_user_must_be_league_user
     league = League.find(params[:id])
