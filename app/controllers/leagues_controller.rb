44da53b141315042f519a45c5d7cc967ccf9905d
Raghu Betina <raghu@firstdraft.com>
2018-05-30 01:07:45 +0000

    Only league user can modify league

--- app/controllers/leagues_controller.rb
+++ app/controllers/leagues_controller.rb
@@ -1,4 +1,14 @@
 class LeaguesController < ApplicationController
+  before_action :current_user_must_be_league_user, :only => [:edit, :update, :destroy]
+
+  def current_user_must_be_league_user
+    league = League.find(params[:id])
+
+    unless current_user == league.user
+      redirect_to :back, :alert => "You are not authorized for that."
+    end
+  end
+
   def index
     @leagues = League.all
 
