57cdb945d977bd90979e27696bfc794c9ff26850
Raghu Betina <raghu@firstdraft.com>
2018-05-30 01:07:41 +0000

    Whitelist columns in League for ActiveAdmin

--- app/admin/leagues.rb
+++ app/admin/leagues.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register League do
+
+ permit_params :user_id, :num_teams, :scoring, :user_first_pick, :league_name
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
