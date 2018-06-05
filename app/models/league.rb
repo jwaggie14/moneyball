e7bd2d6694bf4b54720f64872a1df217cb00a927
James Whiting <jwhiting@chicagobooth.edu>
2018-06-05 19:35:30 +0000

    more validations

--- app/models/league.rb
+++ app/models/league.rb
@@ -16,4 +16,15 @@ class League < ApplicationRecord
 
   # Validations
   validates :league_name, :user_first_pick, :num_teams, presence: true
+  validates :user_first_pick, :num_teams, numericality: { only_integer:true }
+  validates :num_teams, numericality: { greater_than: 1, message: " must be at least 2." }
+  validates :num_teams, numericality: { less_than: 17, message: " must be at most 16." }
+  validates :user_first_pick, numericality: { greater_than: 0, message: " can't be below 1..." }
+  validate :cant_be_more_than_teams
+  
+  
+  def cant_be_more_than_teams
+    errors.add(:num_teams, " can't be bigger than the number of teams...") if num_teams.to_i < user_first_pick.to_i
+  end
+  
 end
